4b2a19c9727c86e0229c750d7d548c6dad087c0d
Ryan Lirio <ryanlirio2@gmail.com>
2024-09-27 13:43:14 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Desafio
+{
+    class Program
+    {
+        static void Main()
+        {
+            Jogador1();
+        }
+        static public void Jogador1()
+        {
+            int tentativa;
+            Console.WriteLine("Escolha um numero de 1 a 100 para o jogador 2 acertar:");
+            int numEscolhido = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Tente advinhar o numero agora");
+                tentativa = int.Parse(Console.ReadLine());
+                if(tentativa < numEscolhido)
+                    Console.WriteLine("Errou! o numero escolhido pelo jogador 1 é maior!");
+                if(tentativa > numEscolhido)
+                    Console.WriteLine("Errou! o numero escolhido pelo jogador 1 é menor!");
+                Thread.Sleep(2000);
+            }while(tentativa != numEscolhido);
+            Console.WriteLine("Acertou!!!");
+
+        }
+    }
+}
